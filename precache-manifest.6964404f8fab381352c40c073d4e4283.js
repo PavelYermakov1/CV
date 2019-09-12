2dd45786e33c84ce472f37c39f4178624c88df06
Pavel Yermakov <pavelak137@mail.ru>
2019-09-13 01:54:02 +0300

    Updates

---
+++ precache-manifest.6964404f8fab381352c40c073d4e4283.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1baf3ce5c24de67e81f1db1a9949e82b",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "361b109376b78f55d8ac",
+    "url": "/CV/static/css/main.d12b1b9a.chunk.css"
+  },
+  {
+    "revision": "f478c3e6a5a59dee73f6",
+    "url": "/CV/static/js/2.81f98875.chunk.js"
+  },
+  {
+    "revision": "361b109376b78f55d8ac",
+    "url": "/CV/static/js/main.c71c4fff.chunk.js"
+  },
+  {
+    "revision": "2cf4702ce8cb77ea0ae4",
+    "url": "/CV/static/js/runtime~main.004ab757.js"
+  },
+  {
+    "revision": "119f18a2d7c8378f94bd0c097158cbd0",
+    "url": "/CV/static/media/headerFoto.119f18a2.jpg"
+  },
+  {
+    "revision": "0416562dcecf3eb546341d0e565272f7",
+    "url": "/CV/static/media/photo.0416562d.jpg"
+  }
+]);
